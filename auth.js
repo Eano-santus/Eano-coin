1320ec1f390763f91772635e532791904d96d919
eano coin <Santorumsantus@gmail.com>
2025-07-05 12:22:43 +0100

    Add auth.js with Firebase initialization and authentication export

---
+++ auth.js
@@ -0,0 +1,8 @@
+// auth.js
+
+// Firebase Config const firebaseConfig = { apiKey: "AIzaSyCzNpblYEjxZvOtuwao3JakP-FaZAT-Upw", authDomain: "eano-miner.firebaseapp.com", projectId: "eano-miner", storageBucket: "eano-miner.appspot.com", messagingSenderId: "50186911438", appId: "1:50186911438:web:85410fccc7c5933d761a9f" };
+
+// Initialize Firebase firebase.initializeApp(firebaseConfig);
+
+// Export Firebase services const auth = firebase.auth(); const db = firebase.firestore();
+
