f33aec2b169bd5e958459e74112bebfd6dd4d3b6
eano coin <Santorumsantus@gmail.com>
2025-07-16 05:58:15 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCzNpblYEjxZvOtuwao3JakP-FaZAT-Upw",
   authDomain: "eano-miner.firebaseapp.com",
   projectId: "eano-miner",
-  storageBucket: "eano-miner.firebasestorage.app",
+  storageBucket: "eano-miner.appspot.com",
   messagingSenderId: "50186911438",
   appId: "1:50186911438:web:85410fccc7c5933d761a9f",
   measurementId: "G-NS0W6QSS69"
